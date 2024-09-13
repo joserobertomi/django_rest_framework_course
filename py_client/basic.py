a07940ee44ead21dc8f84203185d9d0129fc14af
Jose Roberto <joserobertomi@outlook.com>
2024-09-12 22:30:54 -0300

    Django Model Instance as API response

--- py_client/basic.py
+++ py_client/basic.py
@@ -4,7 +4,7 @@ endpoint = "https://httpbin.org/status/200"
 endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/?this_arg=this_value"
 
-get_response = requests.get(endpoint, params={"abs": 123}, json={"query": "Fala comigo bb!!"}) # HTTP request
+get_response = requests.get(endpoint,json={"product_id": 123}) # HTTP request
 print(get_response.text)
 print(get_response.json())
 print(get_response.status_code)
